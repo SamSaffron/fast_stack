3fd1219924b91f26fa2c98371b01f8f098c3ea2b
Sam <sam.saffron@gmail.com>
2013-09-03 12:29:47 +1000

    added a backup collection of stack traces using bg thread, cause in cases of #sleep timer was not firing properly

--- lib/fast_stack.rb
+++ lib/fast_stack.rb
@@ -1,16 +1,45 @@
 require 'fast_stack/fast_stack'
 module FastStack
   def self.profile(millisecs=1, &blk)
+    done = false
     stacks = []
     thread = Thread.current
+    delta = millisecs / 1000.0
+    last_time = Time.new
 
     new_api = thread.respond_to?(:backtrace_locations)
 
-    trap('PROF') do
-      stacks << (new_api ? thread.backtrace_locations : thread.backtrace)
+    # NOTE not extracting to another method to avoid adding a frame
+    #  to the stack
+
+    # this thread catches stuff fast stack does not
+    # the main case is catching sleeping threads
+    profile_thread = Thread.new do
+      until done
+        start = Time.new
+        if last_time < start - delta
+          last_time = start
+          stacks << (new_api ? thread.backtrace_locations : thread.backtrace)
+        end
+
+        sleep(delta)
+      end
     end
 
-    profile_block(millisecs * 1000, &blk)
+    trap('PROF') do
+      start = Time.new
+      if last_time < start - delta
+        last_time = start
+        stacks << (new_api ? thread.backtrace_locations : thread.backtrace)
+      end
+    end
+
+    begin
+      profile_block(millisecs * 1000, &blk)
+    ensure
+      done = true
+      profile_thread.join
+    end
 
     stacks
   end
